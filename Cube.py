f2ff2f96c2c5001de3e6863c8076de5520436072
Zhongjun Jin <markjin1990@markideapad.(none)>
2014-11-16 14:14:09 -0500

    add cube.py

---
+++ Cube.py
@@ -0,0 +1,22 @@
+# Class Cube
+
+class Cube:
+	dimensions = [];
+	partitions = dict();
+	cache = 
+	num_cache = 0;
+
+	def __init__(self, init_partition):
+		partitions = init_partition;
+		dimensions = init_partition.keys();
+
+  def repartition():
+
+	def 
+
+	# Get predicate like A:10,20, B: >= 10 
+  def answerQuery(predicate,cursor):
+		
+	
+
+
