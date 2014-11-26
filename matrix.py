b9989b2c4d07ee9545a21d2f2e05008bfe697c7a
markjin1990 <markjin1990@gmail.com>
2014-11-26 18:20:06 -0500

    Add matrix

---
+++ matrix.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+class Matrix:
+	matrix = [];
+	col = 0;
+	row = 0;
+
+	def __init__(self,row,col,matrix=[]):
+		self.row = row;
+		self.col = col;
+		if not matrix:
+			for i in range(0,self.row):
+				newrow = [];
+				for j in range(0,self.col):
+					newrow.append(0);
+				self.matrix.append(newrow);
+		else:
+			self.matrix = matrix;
+
+	def getMatrix(self):
+		return self.matrix;
+
+	def addOne(self,row,col):
+		self.matrix[row][col] += 1;
+
+	def update(self,row,col,value):
+		self.matrix[row][col] = value;
+
+	def getValue(self,row,col):
+		return self.matrix[row][col];
--- trainer.py
+++ trainer.py
@@ -3,13 +3,62 @@
 # Return [Relation, [attribute_1:[value1,value2], 
 # attribute_2:[value1,value2],...]]
 
+from matrix import Matrix
+
 def convert_to_num(s):
 	try:
 		return int(s)
 	except ValueError:
 		return float(s)
 
-def partAttr(train_set,attr_partition,ifTinyCube):
+def getSetOFfTwo(mylist):
+	mylist = list(set(mylist));
+	setoftwo = [];
+	for i1,value1 in enumerate(mylist):
+		for i1,value2 in enumerate(mylist):
+			if i1>i2:
+				sefoftwo.append([value1,value2]);
+	return setoftwo;
+
+
+
+def spectralClustering(query_attr_set,cluster_num):
+	# Get a set of aggregates mentioned in query_attr_set. E.g. ["SUM(A)@T","SUM(B)@T"]
+	aggr_list = [];
+	# For each aggregate, we have a list of predicate attribute groups, which will be used
+	# for clustering later
+	aggr_predicate_attr_list = [];
+
+	for item in query_attr_set:
+		if item[0] in aggr_list:
+			i = aggr_list.index(item[0]);
+			aggr_predicate_attr_list[i].append(item[1]);
+		else:
+			aggr_list.append(item[0]);
+			aggr_predicate_attr_list[-1].append(item[1]);
+
+	group_info = dict();
+	for index,predicate_attr_set in enumerate(aggr_predicate_attr_list):
+		attr_set = set();
+		for sub_attr_set in predicate_attr_set:
+			attr_set.union(set(sub_attr_set));
+		attr_set = list(attr_set);
+
+		# Create a similarity graph
+		attr_num = len(attr_set);
+		W = Matrix(attr_num,attr_num);
+
+		# Update the similarity graph
+		for sub_attr_set in predicate_attr_set:
+			retlist = getSetOFfTwo(sub_attr_set);
+			for pair in retlist:
+				index1 = attr_set.index(pair[0]);
+				index2 = attr_set.index(pair[1]);
+				W.addOne(index1,index2);
+				W.addOne(index2,index1);
+
+
+def groupAttr(train_set,attr_partition,ifTinyCube):
 	query_attr_set = [];
 	
 	# Get all predicate attributes for each query
@@ -38,27 +87,39 @@ def partAttr(train_set,attr_partition,ifTinyCube):
 			# Get rid of "AND"
 			if query_set:
 				query_set.pop(0);
+
+			query_set = list(set(query_set));
 		
+		# We get ["SUM(A)@T",[B,C]]
+		# 			 ["SUM(A)@T",[B,E]]
+		# 			 ["SUM(A)@T",[D,F]]
 		query_attr_set.append([aggregate+'@'+relation,attr_set]);
 	
-	print(query_attr_set);
-		
+	#print(query_attr_set);
+	
+	# We have a huge Cube instead of several small tinyCubes
 	if not ifTinyCube:
 		for query_attr in query_attr_set:
 			key = query_attr[0];
 			attr = query_attr[1];
+			# If the aggregate is in the attribution grouping, add atrribute->0
 			if key in attr_partition.keys():
 				attr_dict = attr_partition[key];
 				for att in query_attr[1]:
 					attr_dict[att] = 0;
 					
 				attr_partition[key] = attr_dict;
+			# If the aggregate is not in the attribution grouping, put atrribute->0
 			else:
 				attr_dict = dict();
 				for att in query_attr[1]:
 					attr_dict[att] = 0;
 					
 				attr_partition[key] = attr_dict;
+	# We enable tinyCubes
+	else:
+		attr_partition = spectralClustering(query_attr_set,2);
+
 
 def train_parser(query):
 	query_set = query.split(" ");
@@ -94,7 +155,7 @@ def train_parser(query):
 def train(train_set,ifTinyCube,cube_partitions,attr_partition):
 	# Partition the attributes according to workload 
 	# (Put them in same partition if ifTinyCube is false)
-	partAttr(train_set,attr_partition,ifTinyCube);
+	groupAttr(train_set,attr_partition,ifTinyCube);
 	
 	#print(attr_partition);
 	
